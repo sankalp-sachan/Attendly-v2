64407f211a0a64fd18c3e1ab81d2f7ca25bb62b6
Sankalp Sachan <sankalpsachan2007@gmail.com>
2026-03-09 21:01:16 +0530

    fix minor bugs for prod

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.ef8f1s9t8d"
+    "revision": "0.p3suvrvlee8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
